72f458e9da5d8910ebba876d678fa606b11a9cd8
Marcos Antônio <marcosantoniooo826@gmail.com>
2025-08-06 20:03:01 -0300

    feat: add novos icones

--- app/pages/recuperar-senha-code.vue
+++ app/pages/recuperar-senha-code.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { UCard } from '#components'
 import * as z from 'zod'
-import IconLogo from '~/assets/icons/shield-icon.svg?component'
 import CustomInput from '~/components/CustomInput.vue'
 import type { FormSubmitEvent } from '@nuxt/ui'
 import { useTimerPage } from '~/composables/userTimePage'
@@ -22,10 +21,10 @@ let interval: ReturnType<typeof setInterval> | null = null
 
 onMounted(() => {
   updateRemainingTime()
-  
+
   interval = setInterval(() => {
-    updateRemainingTime() 
-    updateDisplay() 
+    updateRemainingTime()
+    updateDisplay()
 
     if (remainingTime.value <= 0 && interval) {
       clearInterval(interval)
@@ -40,14 +39,14 @@ onBeforeUnmount(() => {
 async function onSubmit(event: FormSubmitEvent<Schema>) {
   toast.add({
     title: 'Sucesso!',
-    description: 'Código verificado com sucesso.', 
+    description: 'Código verificado com sucesso.',
   })
 }
 
 async function resendCode() {
   if (!canResend.value) return
-  
-  try {    
+
+  try {
     startTimer()
     if (!interval) {
       interval = setInterval(() => {
@@ -55,8 +54,7 @@ async function resendCode() {
         updateDisplay()
       }, 1000)
     }
-  } catch (error) {
-  }
+  } catch (error) {}
 }
 
 const schema = z.object({
@@ -69,64 +67,81 @@ const state = reactive({
 
 const formRef = ref()
 const isFormValid = computed(() => {
-  return (
-    state.code?.length >= 6 &&
-    !formRef.value?.errors?.code
-  )
+  return state.code?.length >= 6 && !formRef.value?.errors?.code
 })
 </script>
 
 <template>
-    <div class="flex justify-center w-full min-h-dvh bg-[#EAFDF9] p-[20px] py-8">
-        <UCard class="w-full h-fit rounded-[15px] max-w-2xl">   
-            <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">  
-                <div class="flex flex-col items-center gap-1">
-                    <div>
-                        <IconLogo width="57px" />
-                    </div>
-                    <h1 class="text-[#042F2E] font-semibold text-[22px]">Digite o Código</h1>
-                    <p class="text-[#0F766E] text-[14px] font-medium text-center">
-                        Enviamos um código de 6 digitos para <br> seu email
-                    </p>  
-                </div>
+  <div class="flex justify-center w-full min-h-dvh bg-[#EAFDF9] p-[20px] py-8">
+    <UCard class="w-full h-fit rounded-[15px] max-w-2xl">
+      <UForm
+        :schema="schema"
+        :state="state"
+        class="space-y-4"
+        @submit="onSubmit"
+      >
+        <div class="flex flex-col items-center gap-1">
+          <div class="bg-[#0F766E] rounded-[20px] p-2">
+            <LucideShieldQuestionMark
+              :size="42"
+              stroke-width="1"
+              color="#F0F4F4"
+            />
+          </div>
+          <h1 class="text-[#042F2E] font-semibold text-[22px]">
+            Digite o Código
+          </h1>
+          <p class="text-[#0F766E] text-[14px] font-medium text-center">
+            Enviamos um código de 6 digitos para <br />
+            seu email
+          </p>
+        </div>
 
-                <UFormField label="Código de verificação" name="code" v-slot="{ error }">
-                    <CustomInput            
-                        mask="######"
-                        v-model="state.code"
-                        placeholder="000000"
-                        :error="!!error"
-                    />                 
-                </UFormField>
+        <UFormField
+          label="Código de verificação"
+          name="code"
+          v-slot="{ error }"
+        >
+          <CustomInput
+            mask="######"
+            v-model="state.code"
+            placeholder="000000"
+            :error="!!error"
+          />
+        </UFormField>
 
-                <button
-                type="submit"
-                class="font-extrabold text-[16px] w-full h-[50px] rounded-[15px] transition-colors duration-200"
-                :class="isFormValid
-                    ? 'bg-[#134E4A] text-white'
-                    : 'bg-[#134E4AB2] text-[#ffffff70]'"
-                :disabled="!isFormValid"
-                >
-                Verificar Código
-                </button>
+        <button
+          type="submit"
+          class="font-extrabold text-[16px] w-full h-[50px] rounded-[15px] transition-colors duration-200"
+          :class="
+            isFormValid
+              ? 'bg-[#134E4A] text-white'
+              : 'bg-[#134E4AB2] text-[#ffffff70]'
+          "
+          :disabled="!isFormValid"
+        >
+          Verificar Código
+        </button>
 
-                <div class="mt-7">
-                    <p class="text-[#115E59] text-[13px] font-medium text-center">
-                    Não recebeu o código? 
-                        <button 
-                        @click="resendCode"
-                        :disabled="!canResend"
-                        class="font-bold"
-                        :class="canResend ? 'text-[#134E4A] cursor-pointer' : 'text-[#134E4A70] cursor-default'"
-                        >
-                            <span v-if="!canResend">
-                                Reenviar em {{ displayTime }}
-                            </span>
-                            <span v-else>Reenviar código</span>
-                        </button>
-                    </p>
-                </div>
-            </UForm>    
-        </UCard>
-    </div>
+        <div class="mt-7">
+          <p class="text-[#115E59] text-[13px] font-medium text-center">
+            Não recebeu o código?
+            <button
+              @click="resendCode"
+              :disabled="!canResend"
+              class="font-bold"
+              :class="
+                canResend
+                  ? 'text-[#134E4A] cursor-pointer'
+                  : 'text-[#134E4A70] cursor-default'
+              "
+            >
+              <span v-if="!canResend"> Reenviar em {{ displayTime }} </span>
+              <span v-else>Reenviar código</span>
+            </button>
+          </p>
+        </div>
+      </UForm>
+    </UCard>
+  </div>
 </template>
--- app/pages/recuperar-senha-nova.vue
+++ app/pages/recuperar-senha-nova.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
 import { UCard } from '#components'
 import * as z from 'zod'
-import IconLogo from '~/assets/icons/email-icon.svg?component'
+import { ShieldCheck } from 'lucide-vue-next'
 import CustomInput from '~/components/CustomInput.vue'
 import type { FormSubmitEvent } from '@nuxt/ui'
 
-const schema = z.object({
-  password: z.string().min(8, 'A senha deve ter no mínimo 8 caracteres'),
+const schema = z
+  .object({
+    password: z.string().min(8, 'A senha deve ter no mínimo 8 caracteres'),
     password_confirmation: z
-    .string()
-    .min(8, 'A confirmação deve ter no mínimo 8 caracteres'), 
-})
-.refine((data) => data.password === data.password_confirmation, {
+      .string()
+      .min(8, 'A confirmação deve ter no mínimo 8 caracteres'),
+  })
+  .refine((data) => data.password === data.password_confirmation, {
     message: 'As senhas não correspondem',
     path: ['password_confirmation'],
-})
+  })
 
 type Schema = z.output<typeof schema>
 
@@ -27,8 +28,10 @@ const formRef = ref()
 
 const isFormValid = computed(() => {
   return (
-    state.email?.length &&
-    !formRef.value?.errors?.email
+    state.password?.length &&
+    state.password_confirmation?.length &&
+    !formRef.value?.errors?.password &&
+    !formRef.value?.errors?.password_confirmation
   )
 })
 
@@ -41,70 +44,80 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
   if (!isFormValid.value) return
 
   startTimer()
-  
+
   toast.add({
     title: 'Sucesso!',
     description: 'Código enviado para seu e-mail.',
     color: 'success',
   })
-  
+
   router.push('/recuperar-senha-code')
 }
-
 </script>
 
 <template>
-    <div class="flex justify-center w-full min-h-dvh bg-[#EAFDF9] p-[20px] py-8">
-        <UCard class="w-full h-fit rounded-[15px] max-w-2xl">   
-            <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">  
-                <div class="flex flex-col items-center gap-1">
-                    <div>
-                        <IconLogo width="57px" />
-                    </div>
-                    <h1 class="text-[#042F2E] font-semibold text-[22px]">Esqueceu sua senha?</h1>
-                    <p class="text-[#0F766E] text-[14px] font-medium text-center">
-                        Digite seu e-mail e enviaremos um <br> código para redefinir sua senha
-                    </p>  
-                </div>
+  <div class="flex justify-center w-full min-h-dvh bg-[#EAFDF9] p-[20px] py-8">
+    <UCard class="w-full h-fit rounded-[15px] max-w-2xl">
+      <UForm
+        :schema="schema"
+        :state="state"
+        class="space-y-4"
+        @submit="onSubmit"
+      >
+        <div class="flex flex-col items-center gap-1">
+          <div class="bg-[#0F766E] rounded-[20px] p-2">
+            <ShieldCheck :size="42" stroke-width="1" color="#F0F4F4" />
+          </div>
+          <h1 class="text-[#042F2E] font-semibold text-[22px]">
+            Digite sua nova Senha
+          </h1>
+          <p class="text-[#0F766E] text-[14px] font-medium text-center">
+            Mussum Cacilds
+          </p>
+        </div>
 
-                <<UFormField name="password" v-slot="{ error }">
-                  <CustomInput
-                    label="Senha"
-                    v-model="state.password"
-                    type="password"
-                    placeholder="Senha"
-                    :error="!!error"
-                  />
-                </UFormField>
+        <UFormField name="password" v-slot="{ error }">
+          <CustomInput
+            label="Nova Senha"
+            v-model="state.password"
+            type="password"
+            placeholder="Senha"
+            :error="!!error"
+          />
+        </UFormField>
 
-                <UFormField name="password_confirmation" v-slot="{ error }">
-                  <CustomInput
-                    label="Confirme sua Senha"
-                    v-model="state.password_confirmation"
-                    type="password"
-                    placeholder="Confirme sua senha"
-                    :error="!!error"
-                  />
-                </UFormField>
-                
-                <button
-                type="submit"
-                class="font-extrabold text-[16px] w-full h-[50px] rounded-[15px] transition-colors duration-200"
-                :class="isFormValid
-                    ? 'bg-[#134E4A] text-white' 
-                    : 'bg-[#134E4AB2] text-[#ffffff70]'"
-                :disabled="!isFormValid"
-                >
-                Enviar Código
-                </button>
+        <UFormField name="password_confirmation" v-slot="{ error }">
+          <CustomInput
+            label="Confirme sua Senha"
+            v-model="state.password_confirmation"
+            type="password"
+            placeholder="Confirme sua senha"
+            :error="!!error"
+          />
+        </UFormField>
 
-                <div class="mt-7">
-                    <p class="text-[#115E59] text-[13px] font-medium text-center">
-                    Lembrou da senha? 
-                    <NuxtLink to="/login" class="font-bold">Voltar ao login</NuxtLink>
-                    </p>
-                </div>
-            </UForm>    
-        </UCard>
-    </div>
+        <button
+          type="submit"
+          class="font-extrabold text-[16px] w-full h-[50px] rounded-[15px] transition-colors duration-200"
+          :class="
+            isFormValid
+              ? 'bg-[#134E4A] text-white'
+              : 'bg-[#134E4AB2] text-[#ffffff70]'
+          "
+          :disabled="!isFormValid"
+        >
+          Confirmar Senha
+        </button>
+
+        <div class="mt-7">
+          <p class="text-[#115E59] text-[13px] font-medium text-center">
+            Não recebeu o código?
+            <NuxtLink to="/recuperar-senha-code" class="font-bold"
+              >Voltar a recuperação
+            </NuxtLink>
+          </p>
+        </div>
+      </UForm>
+    </UCard>
+  </div>
 </template>
