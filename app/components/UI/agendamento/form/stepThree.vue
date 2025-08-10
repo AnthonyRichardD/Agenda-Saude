cb14e83bf3be12a93ecaddf89cad7de804afc8f8
Anthony <106710005+AnthonyRichardD@users.noreply.github.com>
2025-08-10 18:20:53 -0300

    feat: finalizar fluxo de seleção de data

--- app/components/UI/agendamento/form/stepThree.vue
+++ app/components/UI/agendamento/form/stepThree.vue
@@ -1,8 +1,46 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import type { DateValue } from '@internationalized/date'
+import { CalendarDate, type DateValue } from '@internationalized/date'
+import moment from 'moment'
+
+const useScheduling = useSchedulingStore()
+import * as z from 'zod'
+import type { FormSubmitEvent } from '@nuxt/ui'
+
+const form = ref()
+
+const schema = z.object({
+  date: z.string().min(1, 'Selecione uma data.'),
+})
+
+type Schema = z.output<typeof schema>
+
+const selectedDate = ref<DateValue | undefined>()
+const state = reactive<Partial<Schema>>({
+  date: useScheduling.formData.date || '',
+})
+
+watch(selectedDate, () => {
+  if (selectedDate.value === undefined) {
+    state.date = ''
+  } else {
+    state.date = selectedDate.value.toString()
+  }
+
+  if (form.value) {
+    form.value.validate('date')
+  }
+})
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  useScheduling.formData.date = event.data.date
+  useScheduling.nextStep()
+}
 
 const availableDatesFromApi = ref([
+  '2025-08-11',
+  '2025-08-12',
+  '2025-08-13',
+  '2025-08-14',
   '2025-08-15',
   '2025-08-18',
   '2025-08-22',
@@ -11,8 +49,6 @@ const availableDatesFromApi = ref([
   '2025-09-10',
   '2025-12-09',
 ])
-
-const selectedDate = ref<Date | null>(null)
 
 const availableDatesSet = computed(() => new Set(availableDatesFromApi.value))
 
@@ -37,45 +73,80 @@ const isDateAvailable = (day: DateValue): boolean => {
   const dateString = day.toString()
   return availableDatesSet.value.has(dateString)
 }
+
+onMounted(() => {
+  if (useScheduling.formData.date) {
+    selectedDate.value = new CalendarDate(
+      Number(moment(useScheduling.formData.date).format('YYYY')),
+      Number(moment(useScheduling.formData.date).format('MM')),
+      Number(moment(useScheduling.formData.date).format('DD'))
+    )
+  }
+})
 </script>
 
 <template>
-  <div
-    class="p-4 bg-gray-50 dark:bg-gray-900 min-h-screen flex flex-col items-center"
-  >
-    <div
-      class="w-full max-w-xs bg-white border-2 border-[#99F6E4] p-2 rounded-[15px]"
+  <div>
+    <UForm
+      ref="form"
+      :schema="schema"
+      :state="state"
+      class="space-y-4"
+      @submit="onSubmit"
     >
-      <UCalendar
-        v-model="selectedDate"
-        :is-date-unavailable="isDateUnavailable"
-        :min-date="minDate"
-        :max-date="maxDate"
-        :year-controls="false"
-        locale="pt-BR"
-        class="w-full"
-      >
-        <template #day="{ day }">
-          <UChip
-            :show="isDateAvailable(day)"
-            color="success"
-            size="2xs"
-            class="w-full h-full flex items-center justify-center"
-          >
-            <span>{{ day.day }}</span>
-          </UChip>
-        </template>
-      </UCalendar>
-    </div>
-    <div v-if="selectedDate">
       <div
-        class="mt-6 p-4 bg-blue-100 border-l-4 border-blue-500 text-blue-700 rounded-lg"
+        class="w-full bg-[#FCFFFE] rounded-[15px] border-2 border-[#CCFBF1] p-[20px] flex flex-col justify-center items-center gap-4"
       >
-        <p class="font-semibold">Data selecionada:</p>
-        <p class="text-lg">
-          {{ new Date(selectedDate).toLocaleDateString('pt-BR') }}
-        </p>
+        <h1 class="text-[#042F2E] text-[24px] font-semibold w-full text-start">
+          Escolha a Data
+        </h1>
+        <UFormField class="w-full max-w-xs" name="date">
+          <div
+            class="w-full bg-white border-2 border-[#99F6E4] p-2 rounded-[15px]"
+          >
+            <UCalendar
+              v-model="selectedDate"
+              :is-date-unavailable="isDateUnavailable"
+              :min-date="minDate"
+              :max-date="maxDate"
+              :year-controls="false"
+              locale="pt-BR"
+              class="w-full"
+            >
+              <template #day="{ day }">
+                <UChip
+                  :show="isDateAvailable(day)"
+                  color="success"
+                  size="2xs"
+                  class="w-full h-full flex items-center justify-center"
+                >
+                  <span>{{ day.day }}</span>
+                </UChip>
+              </template>
+            </UCalendar>
+          </div>
+        </UFormField>
+        <div
+          v-if="state.date == ''"
+          class="bg-[#D8E9FF] text-[13px] w-full max-w-xs h-[53px] text-[#1A4592CC] flex justify-center items-center rounded-[15px] p-3 pl-6"
+        >
+          <p>
+            <span class="font-bold"> Dica: </span>
+            <span
+              >Os dias destacados são os únicos disponíveis para este
+              médico.</span
+            >
+          </p>
+        </div>
       </div>
-    </div>
+      <div class="flex justify-end">
+        <UButton
+          type="submit"
+          class="bg-[#134E4A] w-full text-[#F0F4F4] text-[16px] font-extrabold flex justify-center items-center h-[50px] rounded-[15px] hover:bg-[#134E4A]"
+        >
+          Continuar
+        </UButton>
+      </div>
+    </UForm>
   </div>
 </template>
